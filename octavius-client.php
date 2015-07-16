73ef25bec3a8c86dec614a917b3662447d084135
Octavius.rocks <Octavius.rocks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-16 10:50:05 +0000

    new version 1.2.8
    git-svn-id: https://plugins.svn.wordpress.org/octavius-rocks/tags/1.2.8@1200004 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ octavius-client.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ *
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Octavius Rocks
+ * Plugin URI:        http://www.palasthotel.de
+ * Description:       Tacking click paths
+ * Version:           1.2.8
+ * Author:            PALASTHOTEL by Edward
+ * Author URI:        http://www.palasthotel.de
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       octavius-client
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-octavius-client-activator.php
+ */
+function activate_octavius_client() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-octavius-client-activator.php';
+	Octavius_Client_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-octavius-client-deactivator.php
+ */
+function deactivate_octavius_client() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-octavius-client-deactivator.php';
+	Octavius_Client_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_octavius_client' );
+register_deactivation_hook( __FILE__, 'deactivate_octavius_client' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-octavius-client.php';
+
+/**
+ * run octavius client
+ */
+function run_octavius_client() {
+	$plugin = new Octavius_Client();
+	$plugin->run();
+}
+run_octavius_client();
+
+/**
+ * public method to build data attribute
+ */
+function octavius_client_data_builder($assoc_array){
+	$datas = array();
+	if(is_user_logged_in() && is_admin()){
+		$datas[] = "data-evaluate-octavius='true'";
+	}
+	$assoc_array = apply_filters('octavius_rocks_datas', $assoc_array);
+	foreach ($assoc_array as $key => $value) {
+		$datas[] = "data-octavius-".$key."='".$value."'";
+	}
+	return ' '.implode(" ", $datas).' ';
+}
---
+++ public/class-octavius-client-public.php
@@ -0,0 +1,225 @@
+<?php
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    Octavius_Client
+ * @subpackage Octavius_Client/public
+ * @author     PALASTHOTEL by Edward <eb@palasthotel.de>
+ */
+class Octavius_Client_Public {
+
+	/**
+	 * The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 */
+	public function __construct( $plugin_name, $version ) {
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+	}
+
+	/**
+	 * Register the stylesheets for the public-facing side of the site.
+	 *
+	 */
+	public function render_script() {
+
+		$api_key_id = "ph_octavius_api_key";
+		$server_id = "ph_octavius_server";
+		$port_id = "ph_octavius_port";
+
+		$api_key = get_option($api_key_id,'');
+		$server = get_option($server_id, '');
+		$port = get_option($port_id, '');
+
+		?>
+
+		<style type="text/css">
+		#octavius-needed-pixel {
+			height: 0px;
+			width: 0px;
+			overflow: hidden;
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			z-index: -10;
+		}
+		</style>
+		<?php
+		$url = strtok($_SERVER["REQUEST_URI"],'?');
+
+		$type = $this->get_pagetype();
+		$pid = '';
+		if( strpos($type, "archive-") !== 0
+			&& strpos($type, "category") !== 0
+			&& strpos($type, "tag") !== 0
+			&& strpos($type, "tax") !== 0){
+			$pid = $this->get_content_id();
+		}
+
+		$service_url = $server.":".$port."/hit/oc-found/".$api_key."?url=".$url;
+		$service_url.= "&content_id=".$pid;
+		$service_url.= "&pagetype=".$type;
+		$service_url.= "&content_type=".$type;	
+
+		/**
+		 * look for variant tracking
+		 */
+		if(isset($_GET["oc-variant"]) && $_GET["oc-variant"] != ""){
+			/**
+			 * clear text variant
+			 */
+			$service_url.="&variant=".sanitize_text_field($_GET["oc-variant"]);
+		} else {
+			/**
+			 * apply filters for variant
+			 */
+			$variant = apply_filters( 'octavius_rocks_track_variant', '');
+			if($variant != null && $variant != ''){
+				$service_url.="&variant=".$variant;
+			}
+		}
+		/**
+		 * last step is referer data, so if the pixel url gets too long this gets cut out
+		 */
+		$service_url.= "&referer_domain=".parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);	
+		$service_url.= "&referer_path=".parse_url($_SERVER['HTTP_REFERER'], PHP_URL_PATH);
+
+		?>
+		<img id="octavius-needed-pixel" src="<?php echo $service_url; ?>" />
+		<script type="text/javascript">
+		window.OctaviusInit = function(octavius){
+			octavius.config.service = "<?php echo $server; ?>:<?php echo $port; ?>";
+			octavius.api_key = "<?php echo $api_key; ?>";
+		};
+		window.OctaviusOverwrites = function(octavius){
+			octavius.get_parent_id = function(){
+				return "<?php echo $this->get_content_id(); ?>";
+			};
+			octavius.get_pagetype = function(){
+				return "<?php echo $this->get_pagetype(); ?>";
+			};
+		};
+		(function(d){
+			var js, id = 'octavius-script', ref = d.getElementsByTagName('script')[0];
+			if (d.getElementById(id)) {return;}
+			js = d.createElement('script'); js.id = id; js.async = true;
+			js.src = "<?php echo $server; ?>:<?php echo $port; ?>/files/octavius.client.v1.0.js";
+			ref.parentNode.insertBefore(js, ref);
+		}(document));
+		</script>
+		<?php
+	}
+	/**
+	 * return pagetype of page
+	 */
+	private function get_pagetype(){
+		global $wp_query, $wpdb, $post;
+        if ( is_single() ) {
+        	if ( isset( $post->post_type ) ) {
+        		return $post->post_type;
+        	}
+        } elseif ( is_archive() ) {
+
+			if ( is_post_type_archive() ) {
+				$post_type = get_query_var( 'post_type' );
+				if ( is_array( $post_type ) ){
+					$post_type = reset( $post_type );
+				}
+				return 'archive-' . sanitize_html_class( $post_type );
+			} else if ( is_author() ) {
+				return 'author';
+			} elseif ( is_category() ) {
+				return 'category';
+			} else if( is_tag() ){
+				return 'tag';
+			} else if( is_tax() ){
+				return 'tax';
+			}
+        } else if(is_home() || is_front_page()){
+        	return 'home';
+        }
+        return '';
+	}
+	/**
+	 * return id of page
+	 *
+	 */
+	private function get_content_id(){
+		global $wp_query;
+		if ( is_single() || is_page() ) {
+			return $wp_query->get_queried_object_id();
+		} elseif ( is_archive() ) {
+			if ( is_author() ) {
+				$author = $wp_query->get_queried_object();
+				return $author->ID;
+			} elseif ( is_category() ) {
+				$cat = $wp_query->get_queried_object();
+				if ( isset( $cat->term_id ) ) {
+					return $cat->term_id;
+				}
+			} elseif ( is_tag() || is_tax() ) {
+				$term = $wp_query->get_queried_object();
+				if ( isset( $term->term_id ) ) {
+					return $term->term_id;
+				}
+			}
+        }
+        return '';
+	}
+	/**
+	 * show url info
+	 */
+	public function show_url_info(){
+		global $wp_query;
+		if(isset($_GET['octavius']) and $_GET['octavius']=="info"){
+			$info = (object) array();
+			if( is_single() || is_page() ){
+				$info->title = get_the_title();
+				$info->ID = $this->get_content_id();
+			} else if( is_category() || is_tag() ) {
+				if(is_category()){
+					$info->title = "Category: ";
+				} else if(is_tag()){
+					$info->title = "Tag: ";
+				}
+				ob_start();
+				wp_title();
+				$info->title .= ob_get_contents();
+				ob_end_clean();
+			} else if( is_home() || is_front_page() ){
+	        	ob_start();
+				wp_title();
+				$info->title .= ob_get_contents();
+				ob_end_clean();
+	        }
+
+			wp_send_json($info);
+			die;
+
+		}
+	}
+	/**
+	 * add variants to post object for ab testing
+	 */
+	public function add_variants_to_post($post) {
+		/**
+		 * add to post object for easy access
+		 */
+		$post->octavius_variants = $this->variants->get_variants_values($post->ID);
+	}
+
+}
