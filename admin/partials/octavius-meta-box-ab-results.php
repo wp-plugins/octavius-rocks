fe03b8f9bdef152cb9858813384d2284998e9776
Octavius.rocks <Octavius.rocks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-04 12:57:41 +0000

    new version 1.2.9
    git-svn-id: https://plugins.svn.wordpress.org/octavius-rocks/tags/1.2.9@1212579 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/partials/octavius-meta-box-ab-results.php
@@ -0,0 +1,13 @@
+
+<div class="octavius rocks-ab-results-controls">
+	<select class="octavius-rocks-select">
+		<option value="pageview">Pageview</option>
+		<option value="click">Click</option>
+		<option value="">All</option>
+	</select>
+	<a class="button-secondary octavius-rocks-refresh">Refresh</a>
+</div>
+
+<div class="octavius-rocks-ab-results" data-post-id="<?php echo $post->ID; ?>">
+	<p>Chart is loading...</p>
+</div>
